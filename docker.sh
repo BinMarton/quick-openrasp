fc68d521be983d007d37f72ba4f2248af979f094
root <root@4ztvrsr6o4laubap-0002.novalocal>
2023-08-20 10:29:16 +0800

    t

---
+++ docker.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+sudo yum install -y yum-util
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+sudo yum install  -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo systemctl start docker
+sudo docker run hello-world
