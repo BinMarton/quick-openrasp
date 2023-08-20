b05a25f8272c6cf18f784148d1d6fc97a6b098c3
root <root@4ztvrsr6o4laubap-0002.novalocal>
2023-08-20 14:58:33 +0800

    1

--- docker.sh
+++ docker.sh
@@ -2,6 +2,11 @@
 
 sudo yum install -y yum-util
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install  -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install  -y ydocker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo docker run hello-world
+docker compose up -d
+wget -O cloud.tar.gz https://packages.baidu.com/app/openrasp/release/1.3.7/rasp-cloud.tar.gz
+mkdir cloud
+tar -xzvf cloud.tar.gz -C ./cloud
+nohup ./rasp-cloud -d &
