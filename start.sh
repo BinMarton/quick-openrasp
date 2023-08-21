8245d8ce52cb9e6f1f9ec90ed740bb13653c91fe
root <root@4ztvrsr6o4laubap-0002.novalocal>
2023-08-21 14:30:29 +0800

    3

--- start.sh
+++ start.sh
@@ -12,8 +12,8 @@ docker compose up -d
 
 # install & start rasp-cloud
 curl -o cloud.tar.gz https://packages.baidu.com/app/openrasp/release/1.3.7/rasp-cloud.tar.gz
-mkdir cloud
-tar -xzvf cloud.tar.gz -C ./cloud
+tar -xzvf cloud.tar.gz
+mv rasp-cloud-2022-01-28 cloud
 /bin/cp init/app.conf cloud/conf/app.conf -rf
 cd cloud
 ./rasp-cloud -d
