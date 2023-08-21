fbd0c710e57029d094595fe1f81f5564b674d309
root <root@4ztvrsr6o4laubap-0002.novalocal>
2023-08-21 14:29:01 +0800

    2

--- start.sh
+++ start.sh
@@ -7,11 +7,13 @@ sudo yum install  -y ydocker-ce docker-ce-cli containerd.io docker-buildx-plugin
 sudo systemctl start docker
 sudo docker run hello-world
 
-# start rasp backend services
+# start rasp services
 docker compose up -d
 
 # install & start rasp-cloud
-wget -O cloud.tar.gz https://packages.baidu.com/app/openrasp/release/1.3.7/rasp-cloud.tar.gz
+curl -o cloud.tar.gz https://packages.baidu.com/app/openrasp/release/1.3.7/rasp-cloud.tar.gz
 mkdir cloud
 tar -xzvf cloud.tar.gz -C ./cloud
-nohup ./rasp-cloud -d -type=panel &
+/bin/cp init/app.conf cloud/conf/app.conf -rf
+cd cloud
+./rasp-cloud -d
