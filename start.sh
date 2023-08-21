75a6eaeb04c806d4fefad7770a39ca8a7ce427ef
root <root@4ztvrsr6o4laubap-0002.novalocal>
2023-08-21 19:51:30 +0800

    *)安装错误

--- start.sh
+++ start.sh
@@ -1,9 +1,18 @@
 #! /bin/bash
 
 # install & star docker
-sudo yum install -y yum-util
+sudo yum remove docker \
+                  docker-client \
+                  docker-client-latest \
+                  docker-common \
+                  docker-latest \
+                  docker-latest-logrotate \
+                  docker-logrotate \
+                  docker-engine
+                  
+sudo yum install -y yum-utils
 sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum install  -y ydocker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo yum install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo docker run hello-world
 
